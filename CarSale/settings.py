3e80eb891aefbaf1eccf9bd55f39b02d5ea3aef9
shadexcess <sarewuyyr@gmail.com>
2025-05-14 21:26:51 +0300

    Settings Update

--- CarSale/settings.py
+++ CarSale/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^@47s5wy9yxpm+eqz#9@*han$s_*z+5t*9-7xj1e^*#xtj@q45
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
